ef514a09a4eef5cd955ce2e830fa3db3d3a4d431
Shauzab Khan <shauzabkhan693@gmail.com>
2025-02-26 21:30:41 +0530

    initial commit

---
+++ login/src/app/page.js
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Form from "next/form";
+
+export default function Home() {
+  return (
+
+    <center>
+    <div className={styles.form}>
+
+<Form>
+      <h1>Login Form</h1>
+      <br/><br/>
+      <table cellSpacing={20}>
+        <tbody>
+        <tr>
+            <td><h3>Name: </h3></td>
+            <td><input type="text" name="name" id="name"/></td>
+        </tr>
+
+        <tr>
+            <td><h3>Password: </h3></td>
+            <td><input type="password" name="pass" id="pass"/></td>
+        </tr>
+        
+      
+        </tbody>
+      </table>
+      
+
+    
+      <button type="submit" className={styles.btn}>Submit</button>
+    </Form>
+
+    </div>
+    </center>
+    
+  
+
+  );
+}
