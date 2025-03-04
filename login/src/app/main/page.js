0a5ec3a66e2c52d5cbb00e31564c6f11cbcd0347
Shauzab Khan <shauzabkhan693@gmail.com>
2025-03-05 02:51:21 +0530

    Final Commit

--- login/src/app/main/page.js
+++ login/src/app/main/page.js
@@ -1,92 +1,96 @@
 import Form from "next/form";
 import Script from "next/script";
 import Head from "next/head";
-import  "bootstrap/dist/css/bootstrap.min.css"
+import styles from './styles.css'
+
+
 
 export default function Main() { 
   return (
 
-    <><Head>
+    <><head>
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
               crossOrigin="anonymous" />
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
           
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+          
 
-      </Head>
-      <Script
+      </head>
+      <script
               id="bootstrap-cdn"
-              src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" />   
+              src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" />
 
-               
-
-
+    
+            
               <div className="container-fluid">
-    <div className="row flex-nowrap">
-        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-            <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                <a href="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    <span className="fs-5 d-none d-sm-inline">Menu</span>
-                </a>
-                <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-                    <li className="nav-item">
-                        <a href="#" className="nav-link align-middle px-0">
-                        <span className="ms-1 d-none d-sm-inline">Home</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#submenu1" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-                            <span className="ms-1 d-none d-sm-inline">Dashboard</span> 
-                        </a>
+                    <div className="row flex-nowrap">
+                        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 color ">
+                            <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                                <a href="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                                  <span className="fs-5 d-none d-sm-inline">Menu</span>
+                                </a>
+                                <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                                    <li className="nav-item">
+                                        <a href="#" className="nav-link align-middle px-0">
+                                        <span className="ms-1 d-none d-sm-inline text-white"><i class="bi bi-house p-1"></i>Home</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                     <a href="#submenu1" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
+                                          <span className="ms-1 d-none d-sm-inline text-white"><i class="bi bi-app-indicator p-1"></i>Dashboard<i class="bi bi-arrow-down p-1"></i></span> 
+                                     </a>
 
-                        <ul className="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                            <li className="w-100">
-                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 1 </a>
-                            </li>
-                            <li>
-                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 2 </a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#" className="nav-link px-0 align-middle">
-                            <span className="ms-1 d-none d-sm-inline">Orders</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#submenu2" data-bs-toggle="collapse" className="nav-link px-0 align-middle ">
-                            <span className="ms-1 d-none d-sm-inline">Shipping</span>
-                        </a>
-                        <ul className="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
-                            <li className="w-100">
-                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 1</a>
-                            </li>
-                            <li>
-                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 2</a>
-                            </li>
-                        </ul>
-                    </li>
+                                        <ul className="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+                                            <li className="w-100">
+                                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline text-white bg-primary p-2">Item</span> 1 </a>
+                                            </li>
+                                            <li>
+                                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline text-white bg-primary p-2">Item</span> 2 </a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="#" className="nav-link px-0 align-middle">
+                                            <span className="ms-1 d-none d-sm-inline text-white"><i class="bi bi-cart p-1"></i>Orders</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#submenu2" data-bs-toggle="collapse" className="nav-link px-0 align-middle ">
+                                            <span className="ms-1 d-none d-sm-inline text-white"><i class="bi bi-truck p-1"></i>Shipping<i class="bi bi-arrow-down p-1"></i></span>
+                                        </a>
+                                        <ul className="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
+                                            <li className="w-100">
+                                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline text-white bg-primary p-2">Item</span> 1</a>
+                                            </li>
+                                            <li>
+                                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline text-white bg-primary p-2">Item</span> 2</a>
+                                            </li>
+                                        </ul>
+                                    </li>
                     <li>
                         <a href="#submenu3" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
-                            <span className="ms-1 d-none d-sm-inline">Products</span> 
+                            <span className="ms-1 d-none d-sm-inline text-white"><i class="bi bi-box p-1"></i>Products<i class="bi bi-arrow-down p-1"></i></span> 
                         </a>
                             <ul className="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
                             <li className="w-100">
-                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 1</a>
+                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline text-white bg-primary p-2">Product</span> 1</a>
                             </li>
                             <li>
-                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 2</a>
+                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline text-white bg-primary p-2">Product</span> 2</a>
                             </li>
                             <li>
-                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 3</a>
+                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline text-white bg-primary p-2">Product</span> 3</a>
                             </li>
                             <li>
-                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 4</a>
+                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline text-white bg-primary p-2">Product</span> 4</a>
                             </li>
                         </ul>
                     </li>
                     <li>
                         <a href="#" className="nav-link px-0 align-middle">
-                            <span className="ms-1 d-none d-sm-inline">Customers</span> 
+                            <span className="ms-1 d-none d-sm-inline text-white"><i class="bi bi-person-check-fill p-1"></i>Customers</span> 
                         </a>
                     </li>
                 </ul>
@@ -96,12 +100,14 @@ export default function Main() {
         </div>
 
         <div className="col py-3">
-        <h3>Product Summary</h3><br/>
-
+            
+        <h3>Product Summary </h3><br/>
+        
         <table className="table">
             
             <thead>
                 <tr>
+                    <th scope="col"><input type="checkbox"/></th>
                     <th scope="col">ID</th>
                     <th scope="col">Name</th>
                     <th scope="col">Country</th>
@@ -110,19 +116,22 @@ export default function Main() {
             </thead>
             <tbody>
                 <tr>
-                    <th scope="row">1</th>
+                    <th scope="row"><input type="checkbox"/></th>
+                    <td>1</td>
                     <td>Ahmed</td>
                     <td>India</td>
                     <td>A@gmail.com</td>
                 </tr>
                 <tr>
-                    <th scope="row">2</th>
+                    <th scope="row"><input type="checkbox"/></th>
+                    <td>2</td>
                     <td>Jack</td>
                     <td>Canada</td>
                     <td>J@gmail.com</td>
                 </tr>
                 <tr>
-                    <th scope="row">3</th>
+                    <th scope="row"><input type="checkbox"/></th>
+                    <td>3</td>
                     <td>Michael</td>
                     <td>U.S.A</td>
                     <td>M@gmsil.com</td>
--- login/src/app/page.js
+++ login/src/app/page.js
@@ -1,46 +1,79 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Form from "next/form";
+import  "bootstrap/dist/css/bootstrap.min.css"
+import Head from "next/head";
 
 export default function Home() {
   return (
 
+    <><Head>
 
-    <center>
-    <div className={styles.form}>
-      
+    </Head>
 
-<Form>
-      <h1>Login Form</h1>
+    
+
+
+    <div class="container" class="d-flex justify-content-center">
+
+
+
+      <div  className={styles.form}>
+
+      <h1>Techland</h1>
       <br/><br/>
-      <table cellSpacing={20}>
-        <tbody>
-        <tr>
-            <td><h3>Name: </h3></td>
-            <td><input type="text" name="name" id="name"/></td>
-        </tr>
 
-        <tr>
-            <td><h3>Password: </h3></td>
-            <td><input type="password" name="pass" id="pass"/></td>
-        </tr>
+      
+
+      <Form class="row d-flex justify-content-center">
+
+        <div class="form-group">
+        <label htmlFor="exampleInputEmail1">Email</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your email"/>
+        <br/>
+        </div>
+
+        <br/>
+        <div class="form-group">
+          <label for="exampleInputPassword1" class="">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter your password" />
+          </div>
+    
+
+        <div class="form-check mt-2">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+          <label class="form-check-label" for="exampleCheck1 float-start">Remember me</label>
+          <h6 class="float-end vlink link-primary">Forgot Password?</h6>
+          <br/>
+
+          <a href= "/main">
+            <h3 type="submit" class="btn btn-primary btn-lg btn-block mt-3 w-100">Sign In</h3>
+          </a>
+
+          <h6 class="float-start">Not registered yet? </h6>
+          <h6 class="vlink link-primary float-start">&nbsp; Create account</h6>
+
+
         
-      
-        </tbody>
-      </table>
-      
+          
+        </div>
 
-      <a href= "/main">
-      <h3 className={styles.btn}>Submit</h3>
-      </a>
-      
         
     </Form>
 
+
+
+      </div>
+      
+
+
+
     </div>
-    </center>
+
     
-  
+    
+    
+    </>
 
   );
 }
